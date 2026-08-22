0120ad12c7598a13416ef2ed92700bea7d3b0bf0
Tim <98615444+T-imothy@users.noreply.github.com>
2026-08-22 18:15:59 -0500

    ICC/Gunship: add runtime ownership, dialogue, and four-mode rewards

--- Updates/5870_icc_gunship_rewards.sql
+++ Updates/5870_icc_gunship_rewards.sql
@@ -98,3 +98,152 @@ INSERT INTO `gameobject`
 (6319108,202179,673,8,1,-19.8726,-14.1748,33.6377,4.71239,0,0,0.707107,-0.707107,604800,604800);
 
 COMMIT;
+
+-- Runtime fields belong to the world database rather than scriptdev2.sql.
+UPDATE `creature_template`
+SET `RegenerateStats`=`RegenerateStats` & ~2
+WHERE `Entry` IN
+    (36939,36948,37215,37540,
+     36950,36957,36960,36961,36968,36969,36978,36982,37116,37117);
+
+UPDATE `creature_template`
+SET `NpcFlags`=`NpcFlags` | 1
+WHERE `Entry`=37184;
+
+-- Final tested ICC data: gunship
+START TRANSACTION;
+
+-- BroadcastText records absent from the base world database.
+INSERT IGNORE INTO `broadcast_text`
+(`Id`,`Text`,`Text1`,`ChatTypeID`,`LanguageID`,`ConditionID`,`EmotesID`,`Flags`,
+ `SoundEntriesID1`,`SoundEntriesID2`,`EmoteID1`,`EmoteID2`,`EmoteID3`,
+ `EmoteDelay1`,`EmoteDelay2`,`EmoteDelay3`,`VerifiedBuild`) VALUES
+(37677,'Fire up the engines! We got a meetin\' with destiny, lads!','',1,0,0,0,0,16962,0,0,0,0,0,0,0,12340),
+(37680,'By me own beard! HORDE SAILIN\' IN FAST \'N HOT!','',1,0,0,0,0,16965,0,0,0,0,0,0,0,12340),
+(37683,'Not me battle? I dunnae who ye think ye are, mister, but I got a score to settle with Arthas and yer not gettin\' in me way! FIRE ALL GUNS! FIRE! FIRE!','',1,0,0,0,0,16968,0,0,0,0,0,0,0,12340),
+(37689,'Rise up, sons and daughters of the Horde! Today we battle a hated enemy of the Horde! LOK\'TAR OGAR!','',1,0,0,0,0,17087,0,0,0,0,0,0,0,12340),
+(37691,'Kor\'kron, take us out!','',1,0,0,0,0,17088,0,0,0,0,0,0,0,12340),
+(37698,'You will know our business soon. Kor\'kron, ANNIHILATE THEM!','',1,0,0,0,0,17092,0,0,0,0,0,0,0,12340),
+(37699,'Move yer jalopy or we\'ll blow it out of the sky, orc! The Horde\'s got no business here.','',1,0,0,0,0,16969,0,0,0,0,0,0,0,12340),
+(37703,'We\'re taking hull damage, get a sorcerer out here to shut down those cannons!','',1,0,0,0,0,16957,0,0,0,0,0,0,0,12340),
+(37704,'What\'s this then?! Ye won\'t be takin\' this son o\' Ironforge\'s vessel without a fight!','',1,0,0,0,0,16958,0,0,0,0,0,0,0,12340),
+(37705,'Don\'t say I didn\'t warn ya, scoundrels! Onward, brothers and sisters!','',1,0,0,0,0,16959,0,0,0,0,0,0,0,12340),
+(37711,'We\'re taking hull damage, get a battle-mage out here to shut down those cannons!','',1,0,0,0,0,17082,0,0,0,0,0,0,0,12340);
+
+UPDATE `broadcast_text` SET `ChatTypeID`=1
+WHERE `Id` IN (37677,37678,37679,37680,37681,37682,37683,37685,37689,37691,37692,37694,37697,37698,37699,37700,37701,37702,37703,37704,37705,37708,37709,37710,37711,37712,37713);
+
+UPDATE `broadcast_text`
+SET `SoundEntriesID1` = CASE `Id`
+    WHEN 37677 THEN 16962
+    WHEN 37678 THEN 16963
+    WHEN 37679 THEN 16964
+    WHEN 37680 THEN 16965
+    WHEN 37681 THEN 16966
+    WHEN 37682 THEN 16967
+    WHEN 37683 THEN 16968
+    WHEN 37685 THEN 17093
+    WHEN 37689 THEN 17087
+    WHEN 37691 THEN 17088
+    WHEN 37692 THEN 17089
+    WHEN 37694 THEN 17090
+    WHEN 37697 THEN 17091
+    WHEN 37698 THEN 17092
+    WHEN 37699 THEN 16969
+    WHEN 37700 THEN 16954
+    WHEN 37701 THEN 0
+    WHEN 37702 THEN 16956
+    WHEN 37703 THEN 16957
+    WHEN 37704 THEN 16958
+    WHEN 37705 THEN 16959
+    WHEN 37708 THEN 17079
+    WHEN 37709 THEN 0
+    WHEN 37710 THEN 17081
+    WHEN 37711 THEN 17082
+    WHEN 37712 THEN 17083
+    WHEN 37713 THEN 17084
+    ELSE `SoundEntriesID1`
+END
+WHERE `Id` IN (37677,37678,37679,37680,37681,37682,37683,37685,37689,37691,37692,37694,37697,37698,37699,37700,37701,37702,37703,37704,37705,37708,37709,37710,37711,37712,37713);
+
+-- Four-mode reward bindings and pools.
+DELETE FROM `reference_loot_template` WHERE `entry` IN (12036,34251,34263,34275);
+INSERT INTO `reference_loot_template`
+(`entry`,`item`,`ChanceOrQuestChance`,`groupid`,`mincountOrRef`,`maxcount`,`condition_id`,`comments`) VALUES
+(12036,50340,0,1,1,1,0,'Muradin''s Spyglass'),
+(12036,50787,0,1,1,1,0,'Frost Giant''s Cleaver'),
+(12036,50788,0,1,1,1,0,'Bone Drake''s Enameled Boots'),
+(12036,50789,0,1,1,1,0,'Icecrown Rampart Bracers'),
+(12036,50790,0,1,1,1,0,'Abomination''s Bloody Ring'),
+(12036,50791,0,1,1,1,0,'Saronite Gargoyle Cloak'),
+(12036,50792,0,1,1,1,0,'Pauldrons of Lost Hope'),
+(12036,50793,0,1,1,1,0,'Midnight Sun'),
+(12036,50794,0,1,1,1,0,'Neverending Winter'),
+(12036,50795,0,1,1,1,0,'Cord of Dark Suffering'),
+(12036,50796,0,1,1,1,0,'Bracers of Pale Illumination'),
+(12036,50797,0,1,1,1,0,'Ice-Reinforced Vrykul Helm'),
+(34251,49998,0,1,1,1,0,'Shadowvault Slayer''s Cloak'),
+(34251,49999,0,1,1,1,0,'Skeleton Lord''s Circle'),
+(34251,50000,0,1,1,1,0,'Scourge Hunter''s Vambraces'),
+(34251,50001,0,1,1,1,0,'Ikfirus''s Sack of Wonder'),
+(34251,50002,0,1,1,1,0,'Polar Bear Claw Bracers'),
+(34251,50003,0,1,1,1,0,'Boneguard Commander''s Pauldrons'),
+(34251,50005,0,1,1,1,0,'Amulet of the Silent Eulogy'),
+(34251,50006,0,1,1,1,0,'Corp''rethar Ceremonial Crown'),
+(34251,50008,0,1,1,1,0,'Ring of Rapid Ascent'),
+(34251,50009,0,1,1,1,0,'Boots of Unnatural Growth'),
+(34251,50010,0,1,1,1,0,'Waistband of Righteous Fury'),
+(34251,50011,0,1,1,1,0,'Gunship Captain''s Mittens'),
+(34251,50352,0,1,1,1,0,'Corpse Tongue Coin'),
+(34251,50359,0,1,1,1,0,'Althor''s Abacus'),
+(34251,50411,0,1,1,1,0,'Scourgeborne Waraxe'),
+(34263,50345,0,1,1,1,0,'Muradin''s Spyglass'),
+(34263,51906,0,1,1,1,0,'Ice-Reinforced Vrykul Helm'),
+(34263,51907,0,1,1,1,0,'Bracers of Pale Illumination'),
+(34263,51908,0,1,1,1,0,'Cord of Dark Suffering'),
+(34263,51909,0,1,1,1,0,'Neverending Winter'),
+(34263,51910,0,1,1,1,0,'Midnight Sun'),
+(34263,51911,0,1,1,1,0,'Pauldrons of Lost Hope'),
+(34263,51912,0,1,1,1,0,'Saronite Gargoyle Cloak'),
+(34263,51913,0,1,1,1,0,'Abomination''s Bloody Ring'),
+(34263,51914,0,1,1,1,0,'Icecrown Rampart Bracers'),
+(34263,51915,0,1,1,1,0,'Bone Drake''s Enameled Boots'),
+(34263,51916,0,1,1,1,0,'Frost Giant''s Cleaver'),
+(34275,50349,0,1,1,1,0,'Corpse Tongue Coin'),
+(34275,50366,0,1,1,1,0,'Althor''s Abacus'),
+(34275,50653,0,1,1,1,0,'Shadowvault Slayer''s Cloak'),
+(34275,50654,0,1,1,1,0,'Scourgeborne Waraxe'),
+(34275,50655,0,1,1,1,0,'Scourge Hunter''s Vambraces'),
+(34275,50656,0,1,1,1,0,'Ikfirus''s Sack of Wonder'),
+(34275,50657,0,1,1,1,0,'Skeleton Lord''s Circle'),
+(34275,50658,0,1,1,1,0,'Amulet of the Silent Eulogy'),
+(34275,50659,0,1,1,1,0,'Polar Bear Claw Bracers'),
+(34275,50660,0,1,1,1,0,'Boneguard Commander''s Pauldrons'),
+(34275,50661,0,1,1,1,0,'Corp''rethar Ceremonial Crown'),
+(34275,50663,0,1,1,1,0,'Gunship Captain''s Mittens'),
+(34275,50664,0,1,1,1,0,'Ring of Rapid Ascent'),
+(34275,50665,0,1,1,1,0,'Boots of Unnatural Growth'),
+(34275,50667,0,1,1,1,0,'Waistband of Righteous Fury');
+DELETE FROM `gameobject_loot_template` WHERE `entry` IN (28045,28057,28072,28090);
+INSERT INTO `gameobject_loot_template`
+(`entry`,`item`,`ChanceOrQuestChance`,`groupid`,`mincountOrRef`,`maxcount`,`condition_id`,`comments`) VALUES
+(28045,12036,100,0,-12036,2,0,'Gunship Armory - (ReferenceTable)'),
+(28045,49426,100,0,2,2,0,'Gunship Armory - Emblem of Frost'),
+(28057,34263,100,0,-34263,2,0,'Gunship Armory - (ReferenceTable)'),
+(28057,49426,100,0,2,2,0,'Gunship Armory - Emblem of Frost'),
+(28057,49908,38,0,1,1,0,'Gunship Armory - Primordial Saronite'),
+(28072,34251,100,0,-34251,3,0,'Gunship Armory - (ReferenceTable)'),
+(28072,49426,100,0,2,2,0,'Gunship Armory - Emblem of Frost'),
+(28072,49908,38,0,1,1,0,'Gunship Armory - Primordial Saronite'),
+(28072,50274,-35,0,1,1,0,'Gunship Armory - Shadowfrost Shard'),
+(28090,34275,100,0,-34275,3,0,'Gunship Armory - (ReferenceTable)'),
+(28090,49426,100,0,2,2,0,'Gunship Armory - Emblem of Frost'),
+(28090,49908,50,0,1,1,0,'Gunship Armory - Primordial Saronite'),
+(28090,50274,-75,0,1,1,0,'Gunship Armory - Shadowfrost Shard');
+REPLACE INTO `reference_loot_template_names` (`entry`,`name`) VALUES
+(12036,'ICC Gunship 10 Normal'),
+(34251,'ICC Gunship 25 Normal'),
+(34263,'ICC Gunship 10 Heroic'),
+(34275,'ICC Gunship 25 Heroic');
+
+COMMIT;
